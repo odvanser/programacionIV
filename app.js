1212706503589692dd288294acbb17fe17ee3991
odvanser <odvanserrano14@gmail.com>
2020-02-20 15:53:02 -0600

    ajax

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded",e=>{
     document.addEventListener("submit",event=>{
         event.preventDefault();
+        let $resp = document.querySelector("#lblSaludo");
+        nombre = document.querySelector("#txtNombre").Value;
+    
+        $resp.innerHTML= 'iniciando peticion del servee....';
+        
+        fetch('saludo.php').then(resp=>resp.text()).then(resp=>{
+            $resp.innerHTML = 'Hola $(nombre) desde el servidor $(resp)'
+        })
+
 
         
     });
