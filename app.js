e9b008ea808f7db540eddffe36ecd49b7eec315f
odvanser <odvanserrano14@gmail.com>
2020-02-26 09:22:44 -0600

    listo

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded",e=>{
     document.addEventListener("submit",event=>{
         event.preventDefault();
-        let $resp = document.querySelector("#lblSaludo");
-        nombre = document.querySelector("#txtNombre").Value;
-    
-        $resp.innerHTML= 'iniciando peticion del servee....';
-        
-        fetch('saludo.php').then(resp=>resp.text()).then(resp=>{
-            $resp.innerHTML = 'Hola $(nombre) desde el servidor $(resp)'
-        })
+        let $resp = document.querySelector("#lblSaludo"),
+            nombre = document.querySelector("#txtNombre").value;
+        $resp.innerHTML = 'Iniciando Peticion al server...';
 
-
-        
+        fetch(`saludo.php?nombre=${nombre}`).then(resp=>resp.text()).then(resp=>{
+            //$resp.innerHTML = `Hola ${nombre} ${resp}`;
+            $resp.innerHTML = resp;
+        });
     });
 });
