f152bf1c40358ced86752f9c9470704a41310aac
odvanser <odvanserrano14@gmail.com>
2020-03-29 09:00:35 -0600

    ya list

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,28 @@
-var $ = el => document.querySelector(el);
-document.addEventListener("DOMContentLoaded",event=>{
+function init() {
+    var $ = el => {
+        return el.match(/^#/) ? document.querySelector(el) : document.querySelectorAll(el);
+    }
     let mostrarVista = $("[class*='mostrar']");
-    mostrarVista.addEventListener('click',e=>{
-        e.stopPropagation();
+    console.log(mostrarVista);
+    mostrarVista.forEach(element => {
+        element.addEventListener('click', e => {
+            e.stopPropagation();
 
-        let modulo = e.srcElement.dataset.modulo;
-        
-        fetch('public/vistas/alumnos/alumnos.html').then( resp=>resp.text() ).then(resp=>{
-            $(`#vistas-${modulo}`).innerHTML = resp;
-            
-            let btnCerrar = $(".close");
-            btnCerrar.addEventListener("click",event=>{
-                $(`#vistas-${modulo}`).innerHTML = "";
+            let modulo = e.srcElement.dataset.modulo,
+                form = e.srcElement.dataset.form;
+            fetch(`public/vistas/${modulo}/${form}.html`).then(resp => resp.text()).then(resp => {
+                $(`#vistas-${form}`).innerHTML = resp;
+
+                let btnCerrar = $(`#btn-close-${form}`);
+                btnCerrar.addEventListener("click", event => {
+                    $(`#vistas-${form}`).innerHTML = "";
+                });
+                import(`../vistas/${modulo}/${form}.js`).then(module => {
+                    module.modulo();
+                });
+                init();
             });
-
-            let cuerpo = $("body"),
-                script = document.createElement("script");
-            script.src = `public/vistas/${modulo}/${modulo}.js`;
-            cuerpo.appendChild(script);
         });
     });
-});
+}
+init();
